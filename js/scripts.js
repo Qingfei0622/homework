7268a5ef2a656316e311fe1828f50f7934a9720f
Qing Fei <jo20061130@gmail.com>
2026-06-27 01:36:52 +0800

    set title

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
     var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
+         navbarCollapsible = document.body.querySelector('#mainNav');
         if (!navbarCollapsible) {
             return;
         }
